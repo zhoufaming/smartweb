92332938c9286a4cc1d3ceb28a316f5334f7c288
zhoufaming <874051547@qq.com>
2018-07-22 18:55:37 +0800

    Revert "第二次更新"
    This reverts commit 2ad43c2a1b3aef29f63821a5b3feb007f466c6e8.

--- index/views.py
+++ index/views.py
@@ -3,15 +3,15 @@ from __future__ import unicode_literals
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-# def auth(func):
-#     def inner(reqeust,*args,**kwargs):
-#         v = reqeust.COOKIES.get('user')
-#         if not v:
-#             return HttpResponseRedirect('/login/')
-#         return func(reqeust, *args,**kwargs)
-#     return inner
+def auth(func):
+    def inner(reqeust,*args,**kwargs):
+        v = reqeust.COOKIES.get('user')
+        if not v:
+            return HttpResponseRedirect('/login/')
+        return func(reqeust, *args,**kwargs)
+    return inner
 # @login_required(login_url='/login/')
-# @auth
+@auth
 def index_fuc(request):
 	return render (request,'index.html')
 # Create your views here.
--- smartweb/settings.py
+++ smartweb/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'login',
     'index',
     'debug_toolbar',
-    'LogStatistics',
      
 ]
 
